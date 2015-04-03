db562edd04c41c2c30f21a5f6e5fb3ec4396b17c
Izzy <yzziizzy@gmail.com>
2015-04-02 23:56:08 -0700

    increment

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ var fs = require('fs');
 var path = process.argv[2];
 
 
-var source = readFileSync(path)+'';
+var source = fs.readFileSync(path)+'';
 
 try {
 	var ast = jsparser.parse(source);
@@ -17,6 +17,8 @@ try {
 catch(e) {
 	console.log("JS Parse Error: " + e);
 }
+
+
 
 
 function parseFn(ast, parent, fnnode) {
@@ -60,22 +62,130 @@ function parseFn(ast, parent, fnnode) {
 
 
 
+var rules = {
+	Program: null,
+	EmptyStatement: null,
+	BlockStatement: null,
+	ExpressionStatement: null,
+	IfStatement: null,
+	LabeledStatement: null,
+	BreakStatement: null,
+	ContinueStatement: null,
+	WithStatement: null,
+	SwitchStatement: null,
+	ReturnStatement: null,
+	ThrowStatement: null,
+	TryStatement: null,
+	WhileStatement: null,
+	DoWhileStatement: null,
+	ForStatement: null,
+	ForInStatement: null,
+	DebugggerStatement: null,
+	FunctionDeclaration: null,
+	VariableDeclaration: null,
+	VariableDeclarator: null,
+	ThisExpression: null,
+	ArrayExpression: null,
+	ObjectExpression: null,
+	FunctionExpression: null,
+	SequenceExpression: null,
+	UnaryExpression: null,
+	BinaryExpression: null,
+	AssignmentExpression: null,
+	UpdateExpression: null,
+	LogicalExpression: null,
+	ConditionalExpression: null,
+	NewExpression: null,
+	CallExpression: null,
+	MemberExpression: null,
+	SwitchCase: null,
+	CatchClause: null,
+	Identifier: null,
+	Literal: null,
+};
+
+function self(node, rules) { return [node] };
+
+var expRules = {
+	Program: 'body',
+	EmptyStatement: null,
+	BlockStatement: null,
+	ExpressionStatement: null,
+	IfStatement: null,
+	LabeledStatement: null,
+	BreakStatement: null,
+	ContinueStatement: null,
+	WithStatement: null,
+	SwitchStatement: null,
+	ReturnStatement: null,
+	ThrowStatement: null,
+	TryStatement: null,
+	WhileStatement: null,
+	DoWhileStatement: null,
+	ForStatement: null,
+	ForInStatement: null,
+	DebugggerStatement: null,
+	FunctionDeclaration: null,
+	VariableDeclaration: 'declarations',
+	VariableDeclarator: function(node, rules) { return c([node], walkTree(node.init, rules)) }, // hmm, hmm.... need this node and its kids
+	ThisExpression: null,
+	ArrayExpression: null,
+	ObjectExpression: null,
+	FunctionExpression: null,
+	SequenceExpression: null,
+	UnaryExpression: null,
+	BinaryExpression: null,
+	AssignmentExpression: null,
+	UpdateExpression: null,
+	LogicalExpression: null,
+	ConditionalExpression: null,
+	NewExpression: null,
+	CallExpression: null,
+	MemberExpression: null,
+	SwitchCase: null,
+	CatchClause: null,
+	Identifier: null,
+	Literal: null,
+};
 
 
-function walkTree(node) {
-	
 
+
+function walkTree(node, rules) {
 	
+	if(node instanceof Array) {
+		return c(node.map(function(x) { return walkTree(x, rules) }));
+	}
 	
+	var list = [];
+	console.log(node); 
+	console.log('------------');
+	var a = rules[node.type];
+	if(!a) {
+		return [];
+	}
+	else if(a instanceof Array) {
+		return c(a.map(function(x) { return walkTree(node[x], rules) }));
+	}
+	else if(typeof a == 'string') {
+		return walkTree(node[a], rules);
+	}
+	else if(typeof a == 'function') {
+		return f(node, rules);
+	}
+	
+	return [];
 }
 
 
 
+function c() {
+	return Array.prototype.concat.apply(arguments);
+}
 
 
 
-
-
+walkTree(ast, expRules);
 
 
 
---
+++ simple-target.js
@@ -0,0 +1,18 @@
+var a, b;
+var c = 7;
+var d = 9,
+	e = 4;
+	
+function lol(z, y) {
+	var x = 3;
+	
+	var w = x + d;
+	
+	var foo = function(m, n) {
+		return this + n;
+	}
+	
+	foo();
+}
+
+
